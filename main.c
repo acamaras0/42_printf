3c81533a6b09d4027c56f42ea6327437257ef859
Anamaria Camarasan <acamaras@student.hive.fi>
2022-03-03 14:36:00 +0200

    wind of change

--- main.c
+++ main.c
@@ -3,65 +3,83 @@
 
 int     main()
 {
-    char a;
-    a = 'a';
-
-    int b;
-    b = 1;
-
     char *c;
     c = "string";
 
     void *p;
     p = &c;
 
-    unsigned int j;
-    j = 777;
+    printf("*********** B A S I C ***********\n\n");
 
-    int x;
-    x = 526;
-
-    ft_printf("This is a character :%c\n", a);
-    printf ("This is a character*:%c\n", a);
-    ft_printf("******************************\n");
-    ft_printf("This is an integer : %d\n", b);
-    printf("This is an integer*: %d\n", b);
+    ft_printf("This is a character :%c\n", 'a');
+    printf ("This is a character*:%c\n", 'a');
+    ft_printf("______________________________\n\n");
+    ft_printf("This is an integer : %d\n", 1);
+    printf("This is an integer*: %d\n", 1);
     ft_printf("This is an integer : %d\n", -1);
     printf("This is an integer*: %d\n", -1);
-    ft_printf("This is an integer : %li\n", -2147483648);
-    printf("This is an integer*: %li\n", -2147483648);
-    ft_printf("This is an integer : %li\n", 2147483648);
-    printf("This is an integer*: %li\n", 2147483648);
-    ft_printf("******************************\n");
+    ft_printf("______________________________\n\n");
     ft_printf("This is a string   : %s\n", c);
     printf("This is a string*  : %s\n", c);
-    ft_printf("******************************\n");
+    ft_printf("______________________________\n\n");
     ft_printf("This is a percent  : %%\n");
     printf("This is a percent* : %%\n");
-    ft_printf("******************************\n");
+    ft_printf("______________________________\n\n");
     ft_printf("This is a pointer  : %p\n", p);
     printf("This is a pointer* : %p\n", p);
     ft_printf("This is a pointer  : %p\n", NULL);
     printf("This is a pointer* : %p\n", NULL);
-    ft_printf("******************************\n");
-    ft_printf("This is an u int   : %u\n", j);
-    printf("This is an u int*  : %u\n", j);
-    ft_printf("This is an u int   : %u\n", 55165);
-    printf("This is an u int*  : %u\n", 55165);
-    ft_printf("This is neg u int  : %u\n", -7);
-    printf("This is neg u int* : %u\n", -7); 
-    ft_printf("******************************\n");
-    ft_printf("This is a hex  : %x\n", x);
-    printf("This is a hex* : %x\n", x); 
+    ft_printf("______________________________\n\n");
+    ft_printf("This is an u_int   : %u\n", 777);
+    printf("This is an u_int*  : %u\n", 777);
+    ft_printf("This is an u_int   : %u\n", 55165);
+    printf("This is an u_int*  : %u\n", 55165);
+    ft_printf("This is neg u_int  : %u\n", -7);
+    printf("This is neg u_int* : %u\n", -7); 
+    ft_printf("______________________________\n\n");
+    ft_printf("This is a hex  : %x\n", 526);
+    printf("This is a hex* : %x\n", 526); 
     ft_printf("This is a hex  : %X\n", 42);
     printf("This is a hex* : %X\n", 42);
-    ft_printf("******************************\n");
-    ft_printf("This is an octal   : %o\n", x);
-    printf("This is an octal*  : %o\n", x);
+    ft_printf("______________________________\n\n");
+    ft_printf("This is an octal   : %o\n", 526);
+    printf("This is an octal*  : %o\n", 526);
     ft_printf("This is an octal   : %o\n", 46);
     printf("This is an octal*  : %o\n", 46);
     ft_printf("This is an octal   : %o\n", -142);
     printf("This is an octal*  : %o\n", -142);
+
+    printf("\n*********** L E N G T H ***********\n\n");
+
+    ft_printf("This is an li : %li\n", 2147483648);
+    printf("This is an li*: %li\n", 2147483648);
+    ft_printf("This is an li : %li\n", -2147483648);
+    printf("This is an li*: %li\n", -2147483648);
+    ft_printf("______________________________\n\n");
+    ft_printf("This is an ld : %ld\n", 21474836480);
+    printf("This is an ld*: %ld\n", 21474836480);
+    ft_printf("This is an ld : %ld\n", -9223372036854775);
+    printf("This is an ld*: %ld\n", -9223372036854775);
+    ft_printf("______________________________\n\n");
+    ft_printf("This is an lu : %lu\n", 2147483648);
+    printf("This is an lu*: %lu\n", 2147483648);
+    ft_printf("This is an lu : %lu\n", -2147483648);
+    printf("This is an lu*: %lu\n", -2147483648);
+    ft_printf("______________________________\n\n");
+    ft_printf("This is a lx : %lx\n", 21474836480);
+    printf("This is a lx*: %lx\n", 21474836480);
+    ft_printf("This is a lx : %lx\n", -9223372036854775);
+    printf("This is a lx*: %lx\n", -9223372036854775);
+    ft_printf("______________________________\n\n");
+    ft_printf("This is a lX : %lX\n", 21474836480);
+    printf("This is a lX*: %lX\n", 21474836480);
+    ft_printf("This is a lX : %lX\n", -9223372036854775);
+    printf("This is a lX*: %lX\n", -9223372036854775);
+    ft_printf("______________________________\n\n");
+    ft_printf("This is a lo : %lo\n", 2147483648);
+    printf("This is a lo*: %lo\n", 2147483648);
+    ft_printf("This is a lo : %lo\n", -2147483648);
+    printf("This is a lo*: %lo\n", -2147483648);
 
 
 
