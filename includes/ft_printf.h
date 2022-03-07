c7ab46d60e5c83f6c7173988e337186a01dae0a4
Anamaria Camarasan <acamaras@student.hive.fi>
2022-03-07 15:11:35 +0200

    edit

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -28,43 +28,43 @@
 
 typedef struct s_struct
 {
-    va_list arg;
-    const char *form;
-    int print;
-    int length;
-    int index; 
-}   t_struct;
+	va_list arg;
+	const char *form;
+	int print;
+	int length;
+	int index; 
+}				t_struct;
 
 
-void     ifpercent(t_struct *s);
-void     ifchar(t_struct *s, va_list args);
-void     ifstring(t_struct *s, va_list args);
-void     ifnum(t_struct *s, va_list args);
-void     ifpointer(t_struct *s, va_list args);
-void     ifunsigned(t_struct *s, va_list args);
-void     ifhex(t_struct *s, va_list args);
-void     ifhex2(t_struct *s, va_list args);
-void     ifoctal(t_struct *s, va_list args);
+void	ifpercent(t_struct *s);	
+void	ifchar(t_struct *s, va_list args);
+void	ifstring(t_struct *s, va_list args);
+void	ifnum(t_struct *s, va_list args);
+void	ifpointer(t_struct *s, va_list args);
+void	ifunsigned(t_struct *s, va_list args);
+void	ifhex(t_struct *s, va_list args);
+void	ifhex2(t_struct *s, va_list args);
+void	ifoctal(t_struct *s, va_list args);
 
-typedef void     (*t_dispach)(t_struct *s, va_list args);
+typedef void	(*t_dispach)(t_struct *s, va_list args);
 
-static const t_dispach g_dispach[11] = {
-    ifchar,
-    ifstring,
-    ifpointer,
-    ifnum,
-    ifnum,
-    ifoctal,
-    ifunsigned,
-    ifhex2,
-    ifhex,
-    NULL,
+static const	t_dispach g_dispach[11] = {
+	ifchar,
+	ifstring,
+	ifpointer,
+	ifnum,
+	ifnum,
+	ifoctal,
+	ifunsigned,
+	ifhex2,
+	ifhex,
+	NULL,
 };
 
-char    *length_modifiers_int(t_struct *s, unsigned long i, va_list args);
-char    *length_modifiers_uint(t_struct *s, unsigned long i, va_list args);
-char    *length_modifiers_oct(t_struct *s, unsigned long i, va_list args);
-char    *length_modifiers_hex(t_struct *s, unsigned long i, va_list args);
+char	*length_modifiers_int(t_struct *s, unsigned long i, va_list args);
+char	*length_modifiers_uint(t_struct *s, unsigned long i, va_list args);
+char	*length_modifiers_oct(t_struct *s, unsigned long i, va_list args);
+char	*length_modifiers_hex(t_struct *s, unsigned long i, va_list args);
 
 int		ft_printf(const char *format, ...);
 
