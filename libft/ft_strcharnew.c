c4d3def119d5b85a71a914b4c9025318b6bcf978
Anamaria Camarasan <acamaras@student.hive.fi>
2022-03-10 13:02:10 +0200

    new function added

---
+++ libft/ft_strcharnew.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcharnew.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acamaras <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 12:44:21 by acamaras          #+#    #+#             */
+/*   Updated: 2022/03/10 12:44:25 by acamaras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+#include <string.h>
+
+char	*ft_strcharnew(size_t size, int c)
+{
+	size_t	i;
+	char	*s;
+
+	i = 0;
+	s = ft_memalloc(size + 1);
+	if (!s)
+		return (NULL);
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+    s[i] = '\0';
+	return (s);
+}
