26118a97f7c858ab617cdc6bc5acd697a3f8780e
Anamaria Camarasan <acamaras@student.hive.fi>
2022-03-14 14:50:06 +0200

    width added

--- src/hex_and_octal.c
+++ src/hex_and_octal.c
@@ -15,8 +15,14 @@
 void	ifhex(t_struct *s, va_list args)
 {
 	char	*str;
+	int		n;
 
 	str = length_modifiers_hex(s, 0, args);
+	n = ft_strlen(str);
+	if (s->minus == 1 && n > 0)
+		str = align_to_right(s, str);
+	else if (s->minus == 0 && n > 0)
+		str = align_to_left(s, str);
 	ft_putstr(str);
 	s->print += ft_strlen(str);
 	ft_strdel(&str);
@@ -26,6 +32,7 @@ void	ifhex2(t_struct *s, va_list args)
 {
 	char	*str;
 	int		j;
+	int		n;
 
 	j = 0;
 	str = length_modifiers_hex(s, 0, args);
@@ -35,6 +42,11 @@ void	ifhex2(t_struct *s, va_list args)
 			str[j] += 32;
 		j++;
 	}
+	n = ft_strlen(str);
+	if (s->minus == 1 && n > 0)
+		str = align_to_right(s, str);
+	else if (s->minus == 0 && n > 0)
+		str = align_to_left(s, str);
 	ft_putstr(str);
 	s->print += ft_strlen(str);
 	ft_strdel(&str);
@@ -43,8 +55,14 @@ void	ifhex2(t_struct *s, va_list args)
 void	ifoctal(t_struct *s, va_list args)
 {
 	char	*str;
+	int n;
 
 	str = length_modifiers_oct(s, 0, args);
+	n = ft_strlen(str);
+	if (s->minus == 1 && n > 0)
+		str = align_to_right(s, str);
+	else if (s->minus == 0 && n > 0)
+		str = align_to_left(s, str);
 	s->print += ft_strlen(str);
 	ft_putstr(str);
 	ft_strdel(&str);
