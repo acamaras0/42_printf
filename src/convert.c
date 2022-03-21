3ac40b1f9fea9012810b237ba618b4edc7bbf3da
Anamaria Camarasan <acamaras@student.hive.fi>
2022-03-21 14:09:20 +0200

    3/182

--- src/convert.c
+++ src/convert.c
@@ -12,7 +12,20 @@
 
 #include "../includes/ft_printf.h"
 
-char	*convert_left(char *str, int i, char c)
+char	*swap_signs(char *str, char c, int i)
+{
+	while (str[i] && str[i] != '+' && str[i] != '-')
+		i++;
+	if(str[i] == '+' || str[i] == '-')
+	{
+		c = str[i];
+		str[i] = '0';
+		str[0] = c;
+	}
+	return (str);
+}
+
+char	*convert_left(t_struct *s, char *str, int i, char c)
 {
 	char	*temp;
 	char	*joined;
@@ -21,8 +34,14 @@ char	*convert_left(char *str, int i, char c)
 	n = 0;
 	temp = ft_strcharnew(i, c);
 	joined = ft_strjoin(temp, str);
-	ft_strdel(&temp);
-	ft_strdel(&str);
+	if (s->zero && s->width && ((s->plus || s->minus) || s->negative))
+		joined = swap_signs(joined, c, i);
+	//printf("\nHERE: %d\n", i);
+	if (!temp || !str)
+	{
+		ft_strdel(&temp);
+		ft_strdel(&str);
+	}
 	return (joined);
 }
 
@@ -33,23 +52,25 @@ char	*align_to_left(t_struct *s, char *str)
 
 	joined = NULL;
 	i = s->width - ft_strlen(str);
+	if (i > 0 && s->zero == 1 && s->precision > 0 && !(s->hash && s->space && s->plus && s->minus))
+		joined = convert_left(s, str, i , ' ');
 	if (i > 0 && s->zero == 0 && s->negative == 0)
-		return (joined = convert_left(str, i , ' '));
+		return (joined = convert_left(s, str, i , ' '));
 	else if (i > 0 && s->zero == 0 && s->negative == 1 && s->plus == 0)
-		return (joined = convert_left(str, i - 2 , ' '));
+		return (joined = convert_left(s, str, i - 2 , ' '));
 	else if (i > 0 && s->zero == 0 && s->negative == 1 && s->plus == 1)
-		return (joined = convert_left(str, i - 1, ' '));
+		return (joined = convert_left(s, str, i - 1, ' '));
 
 
 
-	else if (i > 0 && s->plus == 0 && s->negative == 0 && s->hash == 0)
-		return (joined = convert_left(str, i , '0'));
-	else if (i > 0 && (s->negative == 1 || s->plus == 1) && s->hash == 0)
-		return (joined = convert_left(str, i - 1, '0'));
-	else if (i > 0 && s->hash == 1)
-		return (joined = convert_left(str, i - 2, '0'));
-	else if (i > 0 && s->hash == 0)
-		return (joined = convert_left(str, i, '0'));
+	else if (i > 0 && s->plus == 0 && s->negative == 0 && s->hash == 0 && !s->precision)
+		return (joined = convert_left(s, str, i , '0'));
+	else if (i > 0 && (s->negative == 1 || s->plus == 1) && s->hash == 0 && !s->precision)
+		return (joined = convert_left(s, str, i, '0'));
+	else if (i > 0 && s->hash == 1 && !s->precision)
+		return (joined = convert_left(s, str, i - 2, '0'));
+	else if (i > 0 && s->hash == 0 && !s->precision)
+		return (joined = convert_left(s, str, i, '0'));
 	return (str);
 }
 
