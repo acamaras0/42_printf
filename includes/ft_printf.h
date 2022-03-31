7df5e1a5c18229224598c1ce001d3f15e167b8c6
Anamaria Camarasan <acamaras@student.hive.fi>
2022-03-31 16:55:18 +0300

    changes

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -77,6 +77,8 @@ void	put_zeroes(t_struct *s, int n);
 void	put_spaces(t_struct *s, int n);
 char	*to_lower(char *str);
 char	*add_zero_plus_minus(char *str, t_struct *s, char c, int conv);
+char	*swap_signs(t_struct *s, char *str, char c, int i);
+void	set_default(t_struct *s);
 
 char	*align_to_left(t_struct *s, char *str);
 char	*align_to_right(t_struct *s, char *str);
